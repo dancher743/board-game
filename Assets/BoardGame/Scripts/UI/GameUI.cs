e8239d1343048d5bf73affa85a8bf3d7063db454
Daniil Dubovik <daniil.dubovik@gmail.com>
2023-01-06 16:45:30 +0300

    Added `GameUI` script

---
+++ Assets/BoardGame/Scripts/UI/GameUI.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace BoardGame.UI
+{
+    public class GameUI : MonoBehaviour
+    {
+        public UnityAction DiceButtonClicked;
+
+        [SerializeField]
+        private Button diceButton;
+
+        [SerializeField]
+        private TextMeshProUGUI statusText;
+
+        private void Awake()
+        {
+            diceButton.onClick.AddListener(() => DiceButtonClicked?.Invoke());
+        }
+
+        private void OnDestroy()
+        {
+            Clear();
+        }
+
+        public void SetStatus(string text)
+        {
+            statusText.text = text;
+        }
+
+        public void SetStatusTextActive(bool value)
+        {
+            statusText.gameObject.SetActive(value);
+        }
+
+        public void Clear()
+        {
+            statusText.text = string.Empty;
+            diceButton.onClick.RemoveAllListeners();
+        }
+    }
+}
