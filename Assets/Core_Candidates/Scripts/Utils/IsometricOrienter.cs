d7db4a9ecf66b43e712d5d546015a285681d3da6
Daniil Dubovik <daniil.dubovik@gmail.com>
2023-01-15 00:04:02 +0300

    Added custom editor `IsometricOrienterEditor` for `IsometricOrienter` component

--- Assets/Core_Candidates/Scripts/Utils/IsometricOrienter.cs
+++ Assets/Core_Candidates/Scripts/Utils/IsometricOrienter.cs
@@ -6,20 +6,10 @@ namespace Core.Utils
     {
         private readonly Vector3 isometricVector = new Vector3(30f, 40f, 0f);
 
-        private void Reset()
-        {
-            Orient();
-        }
-
-        private void Awake()
-        {
-            Orient();
-        }
-
-        private void Orient()
+        public void Orient()
         {
             transform.rotation = Quaternion.Euler(isometricVector);
-            
+
             Debug.Log($"{nameof(IsometricOrienter)}: game object \"{name}\" has been oriented to isometric view.");
         }
     }
