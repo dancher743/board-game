15b284e5db2ba02e65e176bb1f81d5bd5bc7dae8
Daniil Dubovik <daniil.dubovik@gmail.com>
2023-01-15 00:20:53 +0300

    Fixed typo in `IsometricOrienterEditor`'s name

--- Assets/Core_Candidates/Scripts/Utils/IsometricOrienteer.cs
+++ Assets/Core_Candidates/Scripts/Utils/IsometricOrienteer.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Core.Utils
 {
-    public class IsometricOrienter : MonoBehaviour
+    public class IsometricOrienteer : MonoBehaviour
     {
         private readonly Vector3 isometricVector = new Vector3(30f, 40f, 0f);
 
@@ -10,7 +10,7 @@ namespace Core.Utils
         {
             transform.rotation = Quaternion.Euler(isometricVector);
 
-            Debug.Log($"{nameof(IsometricOrienter)}: game object \"{name}\" has been oriented to isometric view.");
+            Debug.Log($"{nameof(IsometricOrienteer)}: game object \"{name}\" has been oriented to isometric view.");
         }
     }
 }
