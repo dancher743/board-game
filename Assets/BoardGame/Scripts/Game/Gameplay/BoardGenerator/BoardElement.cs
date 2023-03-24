f26c5828d6d48875959d0f55326d95ee56ecf830
Daniil Dubovik <daniil.dubovik@gmail.com>
2023-03-24 20:25:01 +0300

    Refactoring

--- Assets/BoardGame/Scripts/Game/Gameplay/BoardGenerator/BoardElement.cs
+++ Assets/BoardGame/Scripts/Game/Gameplay/BoardGenerator/BoardElement.cs
@@ -10,7 +10,7 @@ namespace BoardGame.Configs
         public Cell Prefab { get; private set; }
 
         [field: SerializeField]
-        public Vector2Int RandomCount { get; private set; }
+        public Vector2Int RandomAmount { get; private set; }
 
         [field: SerializeField]
         public bool IsConnector { get; private set; }
---
+++ Assets/BoardGame/Scripts/Game/Gameplay/BoardGenerator/CellsGenerator.cs
@@ -0,0 +1,54 @@
+using BoardGame.Configs;
+using Core;
+using NaughtyAttributes;
+using System.Linq;
+using UnityEngine;
+
+namespace BoardGame.Utils
+{
+    public class CellsGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private BoardElement[] elements;
+
+        [SerializeField]
+        private int elementsAmount;
+
+        [Button]
+        public void Generate()
+        {
+            if (elementsAmount == 0)
+            {
+                return;
+            }
+
+            transform.DestroyChildrens();
+
+            var cellsAmount = elementsAmount;
+            var isPreviousElementConnector = false;
+            var nextCellPosition = Vector3.zero;
+
+            while (cellsAmount > 0)
+            {
+                var boardElements = elements.Where(element => element.IsConnector == !isPreviousElementConnector);
+                var boardElement = boardElements.ElementAt(Random.Range(0, boardElements.Count()));
+                var boardElementAmount = Random.Range(boardElement.RandomAmount.x, boardElement.RandomAmount.y);
+                isPreviousElementConnector = boardElement.IsConnector;
+                cellsAmount -= boardElementAmount;
+
+                if (cellsAmount < 0)
+                {
+                    boardElementAmount -= Mathf.Abs(cellsAmount);
+                    cellsAmount = 0;
+                }
+
+                for (int i = 0; i < boardElementAmount; i++)
+                {
+                    var cell = Instantiate(boardElement.Prefab, transform);
+                    cell.transform.position = nextCellPosition;
+                    nextCellPosition = cell.transform.position + cell.transform.forward;
+                }
+            }
+        }
+    }
+}
